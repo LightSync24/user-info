a7bab87670b78d3fc9a8627bff53eba108353bad
John Doe <johndoe@email.com>
2024-02-10 12:24:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?seed=abc")
+      .then((response) => {
+        const data = response.data.results[0];
+        setImage(data.picture.large);
+        setFirstName(data.name.first);
+        setLastName(data.name.last);
+        setGender(data.gender);
+        setPhone(data.phone);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-container">
+      <div className="inner-container">
+        <img src={image} alt="User Image" className="image" />
+        <div>
+          <div className="name">
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+          </div>
+          <p>{gender}</p>
+          <p>{phone}</p>
+        </div>
+      </div>
     </div>
   );
 }
